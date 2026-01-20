6fe391951513631daf43ee6a4db0323b85c35aaa
zairjmg <jair.jmgu@gmail.com>
2026-01-20 14:47:08 -0500

    fontawesome

--- src/components/saludo.tsx
+++ src/components/saludo.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './saludo.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function Saludo() {
     return (
-        <button id='magic' className='btn'>magic</button>
+        <button id='magic' className='btn'>
+            magic <FontAwesomeIcon icon={faCoffee} />
+        </button>
     );
 }
 
